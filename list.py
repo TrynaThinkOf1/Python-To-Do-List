6b77f5251123af4a85a117c10c5147e7226e288c
TrynaThinkOf1 <zeviberlin@gmail.com>
2024-11-22 12:20:32 -0500

    working

--- list.py
+++ list.py
@@ -1,1 +1,6 @@
+
 to_dos = {}
+
+def list_keys():
+    keys = to_dos.keys()
+    print(list(keys))
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import list
 
-from flask import Flask, redirect, request, render_template
+from flask import Flask, redirect, request, render_template, jsonify
 
 site = Flask(__name__)
 
@@ -12,8 +12,13 @@ def index():
 def add():
     name = request.args.get('name')
     desc = request.args.get('desc')
-    list.to_dos[name:desc]
+    print(name, desc)
+    list.to_dos[name] = desc
     return redirect("/")
+
+@site.route('/show', methods=['GET'])
+def show_todos():
+    return jsonify(list.to_dos)
 
 if __name__ == "__main__":
     site.run(debug=True)
